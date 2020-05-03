e9a3ad69b4933e6f3c3ddcd4b7f1a5e68c1f168f
Abdul Ghani <abdul.ghani@volteo.com>
2020-05-04 01:38:28 +0530

    first release

--- android/app/src/main/java/com/coronaapp/MainApplication.java
+++ android/app/src/main/java/com/coronaapp/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.burnweb.rnsendintent.RNSendIntentPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
